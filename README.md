51601b5f42d4070c45ee1b3c18ee410e363a529c
vmanav <zmanav.1999@gmail.com>
2019-07-21 10:32:41 +0530

    Added README.md file

---
+++ README.md
@@ -0,0 +1,12 @@
+# Chat.IO
+---
+A real time chat application using [sockets](https://socket.io/) and [nodeJS](https://nodejs.org/en/).
+
+### Basic Installation
+Install npm dependencies using:
+`npm i`
+
+visit here: [~~*Chat.IO*~~](#, "Link will be availible soon.")
+
+---
+[Manav Verma](https://github.com/vmanav)
--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-// NOTE:
-// Same functioning like dhruv's code just his is nicely written
-
 const express = require('express')
 const socket = require('socket.io')
-const http = require('http')
-// http is present in node library
+const http = require('http')    // http is present in node library
+
+// specifying heroku's env.PORT
+const PORT = process.env.PORT || 4848;
 
 const app = express()
 const server = http.createServer(app)
@@ -22,25 +21,13 @@ io.on('connection', (socket) => {
     socket.emit('connected')
 
     socket.on('login', (data) => {
-        //username of the user is in data.user
-        // storing the socket id in 'usersockets'
         usersockets[data.user] = socket.id
         console.log(usersockets)
     })
 
     socket.on("send_chat", (data) => {
 
-        // Old code  -->
-        // ---------------------------------------------------
-        // console.log("Data Recieved =>",data.username, " : ", data.message)
-        // // emit to eveyone
-        // io.emit("recieve_chat", {
-        //     message: data.message,
-        //     username: data.username
-        // })
-        // ---------------------------------------------------
-
-        // Adding the private message functionality -->
+        // private message functionality -->
         if (data.message.startsWith('@')) {
             // suppose, data.message = "@a: hello", we have to get 'a' from it.
             // split at :, then remove '@' from begining
@@ -52,7 +39,7 @@ io.on('connection', (socket) => {
             })
         }
         else {
-            socket.broadcast.emit("recieve_chat", {
+            io.emit("recieve_chat", {
                 message: data.message,
                 username: data.username
             })
@@ -60,12 +47,6 @@ io.on('connection', (socket) => {
     })
 })
 
-server.listen(process.env.PORT || 4848, () => {
+server.listen(PORT, () => {
     console.log("Server started on http://localhost:4848")
 })
-
-// HomeWork ->
-
-// MODEL :- chat messages: usernam,mssg
-// mapping between username and socketID in backend
-// sending to a particuar socketID
